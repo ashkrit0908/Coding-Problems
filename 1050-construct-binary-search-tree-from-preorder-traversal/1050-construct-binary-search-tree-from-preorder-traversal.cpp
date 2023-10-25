b3585a3e6258d336052840f8cf3caa807564fce6
Ashkrit Shukla <60595434+ashkrit0908@users.noreply.github.com>
2023-10-25 16:13:50 +0530

    Time: 0 ms (100%), Space: 14.4 MB (13.76%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -12,34 +12,26 @@
 class Solution {
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-         TreeNode* root=new TreeNode(preorder[0]);
-         TreeNode* tempRoot = root;
-         for(int i=1; i<preorder.size(); i++){
-             TreeNode* temp = new TreeNode(preorder[i]);
-             root = tempRoot;
-             while(root != NULL){
-                if(temp->val < root->val){ 
-                    {
-                        if(root->left == NULL){
-                            root->left = temp;
-                            break;
-                        }
-                    }      
-                    root = root->left;
-                }
-                else if(temp->val > root->val){ 
-                    {
-                        if(root->right == NULL){
-                            root->right = temp;
-                            break;
-                        }
-                    }      
-                    root = root->right;
-                }
-             }
-             
-         }
-         return tempRoot;
-
+        vector<int> inorder=preorder;
+        sort(inorder.begin(), inorder.end());
+        map<int, int> mp;
+        for(int i = 0; i< inorder.size(); i++){
+            mp[inorder[i]]=i;
+        }
+        TreeNode *root = built(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, mp);
+        return root;
     }
+    TreeNode* built(vector<int>&preorder, int pstart, int pend, vector<int>&inorder, int istart, int iend, map<int, int>&mp){
+        if(pstart>pend||istart>iend)return NULL;
+
+        TreeNode * root = new TreeNode(preorder[pstart]);
+        int indxroot=mp[root->val];
+        int leftnum=indxroot-istart;
+
+        root->left = built(preorder, pstart+1, pstart+leftnum, inorder, istart, indxroot-1, mp);
+        root->right = built(preorder, pstart+leftnum+1, pend, inorder, indxroot+1, iend, mp);
+
+        return root;
+    }
+
 };
